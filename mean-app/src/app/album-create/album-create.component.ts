dfc97b56369e2cd6e9b9662a3ae8b317629b81d9
Owen Cummings <owenpatrickcummings@gmail.com>
2017-11-02 12:10:49 -0400

    added create review component
    added create review component but routing isn't working quite yet

---
+++ mean-app/src/app/album-create/album-create.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlbumService } from '../Album.service';
+
+@Component({
+  selector: 'app-album-create',
+  templateUrl: './album-create.component.html',
+  styleUrls: ['./album-create.component.css']
+})
+export class AlbumCreateComponent implements OnInit {
+
+  album = {};
+
+  constructor(private albumService: AlbumService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  saveAlbum() {
+    this.albumService.saveAlbum(this.album).then((result) => {
+      //let id = result['_id'];
+      //this.router.navigate(['/book-details', id]);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
@@ -8,17 +8,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AlbumService } from './album.service';
 import { AlbumComponent } from './album/album.component';
+import { AlbumCreateComponent } from './album-create/album-create.component';
 
 const ROUTES = [
-  { path: '', redirectTo: 'albums', pathMatch: 'full' },
-  { path: 'albums', component: AlbumComponent }
+  { path: 'albums', component: AlbumComponent },
+  { path: 'album-create', component: AlbumCreateComponent },
+  { path: '', redirectTo: '/albums', pathMatch: 'full' },
+  { path: '**', redirectTo: '/albums', pathMatch: 'full' },
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumComponent
+    AlbumComponent,
+    AlbumCreateComponent
   ],
   imports: [
     BrowserModule,
