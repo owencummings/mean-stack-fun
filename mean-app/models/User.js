d365c36eb936d01bc62ed65b2f543c70b96a3a01
Owen Cummings <owenpatrickcummings@gmail.com>
2017-11-09 01:20:34 -0500

    amidst adding user auth tokens & hashing
    user auth tokens & hashing

--- mean-app/models/User.js
+++ mean-app/models/User.js
@@ -1,4 +1,8 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var UserSchema = new mongoose.Schema({
   username: String,
@@ -7,6 +11,31 @@ var UserSchema = new mongoose.Schema({
   //review: String,
   updated_at: { type: Date, default: Date.now }, //last logged in
 });
+
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET");
+};
+
+
 
 
 module.exports = mongoose.model('User', UserSchema);
